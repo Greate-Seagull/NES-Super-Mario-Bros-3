73651adc34be1f64660e0319b4fcd767f877c02d
Ryder Rhythm <doanryderrhythm@outlook.com>
2025-04-15 22:08:18 +0700

    #12 - Created hidden map in World 1-1, revamped the constructor for Pipes

--- NES-Super-Mario-Bros-3/Pipe.cpp
+++ NES-Super-Mario-Bros-3/Pipe.cpp
@@ -2,15 +2,63 @@
 
 void CPipe::Render()
 {
-	CAnimations* animations = CAnimations::GetInstance();
-	animations->Get(ani_id)->Render(x, y);
-	RenderBoundingBox();
+	if (this->height <= 0) return;
+	CSprites* s = CSprites::GetInstance();
+
+	float xx = x - cell_width / 2;
+	
+	if (face_direction == 1) //UP
+	{
+		float yy = y + cell_height;
+
+		for (int i = 1; i < this->height; i++)
+		{
+			CSprites* s = CSprites::GetInstance();
+
+			s->Get(this->spriteIdBeginEnd)->Draw(xx, yy);
+			s->Get(this->spriteIdEndEnd)->Draw(xx + cell_width, yy);
+
+			yy -= cell_height;
+		}
+
+		s->Get(this->spriteIdBeginBegin)->Draw(xx, yy);
+		s->Get(this->spriteIdEndBegin)->Draw(xx + cell_width, yy);
+	}
+	else if (face_direction == -1) //DOWN
+	{
+		float yy = y - cell_height;
+		
+		for (int i = 1; i < this->height; i++)
+		{
+			CSprites* s = CSprites::GetInstance();
+
+			s->Get(this->spriteIdBeginEnd)->Draw(xx, yy);
+			s->Get(this->spriteIdEndEnd)->Draw(xx + cell_width, yy);
+
+			yy += cell_height;
+		}
+
+		s->Get(this->spriteIdBeginBegin)->Draw(xx, yy);
+		s->Get(this->spriteIdEndBegin)->Draw(xx + cell_width, yy);
+	}
+
+	//RenderBoundingBox();
 }
 
 void CPipe::GetBoundingBox(float& l, float& t, float& r, float& b)
 {
-	l = x - PIPE_BBOX_WIDTH / 2;
-	t = y - pipe_height / 2;
-	r = l + PIPE_BBOX_WIDTH;
-	b = t + pipe_height;
+	if (this->face_direction == 1)
+	{
+		l = x - PIPE_BBOX_WIDTH / 2;
+		b = y + cell_height * 1.5;
+		r = l + PIPE_BBOX_WIDTH;
+		t = b - height * cell_height;
+	}
+	else if (this->face_direction == -1)
+	{
+		l = x - PIPE_BBOX_WIDTH / 2;
+		t = y - cell_height * 1.5;
+		r = l + PIPE_BBOX_WIDTH;
+		b = t + height * cell_height;
+	}
 }
