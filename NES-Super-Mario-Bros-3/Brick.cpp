955c6829f3b47ea96ef60d45406c4ebfd2bcfc93
Ryder Rhythm <doanryderrhythm@outlook.com>
2025-04-23 21:35:59 +0700

    Implemented necessary features for bricks

--- NES-Super-Mario-Bros-3/Brick.cpp
+++ NES-Super-Mario-Bros-3/Brick.cpp
@@ -1,10 +1,120 @@
 #include "Brick.h"
+#include "PlayScene.h"
+#include "PButton.h"
 
-CBrick::CBrick(float x, float y):
-	CQuestionBlock(x, y)
+#include "AssetIDs.h"
+
+#include "Animation.h"
+#include "Animations.h"
+
+CBrick::CBrick(float x, float y, int itemID):
+	CBlock(x, y)
 {
 	bbox_width = BRICK_BBOX_WIDTH;
 	bbox_height = BRICK_BBOX_HEIGHT;
 
 	aniID = ID_ANI_BRICK;
+
+	this->itemID = itemID;
+	this->origin_y = y;
+
+	SetState(STATE_LIVE);
+	SetBoundingBox(BRICK_BBOX_WIDTH, BRICK_BBOX_HEIGHT);
+}
+
+void CBrick::Render()
+{
+	if (state == STATE_DIE && itemID == 45) aniID = ID_ANI_BRICK_DIE;
+	else aniID = ID_ANI_BRICK;
+	CAnimations::GetInstance()->Get(aniID)->Render(x, y);
+
+	RenderBoundingBox();
+}
+
+void CBrick::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+	InPhase(dt, coObjects);
+}
+
+void CBrick::InPhase(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+	switch (state)
+	{
+	case BRICK_STATE_TOGGLE:
+		Shaking(dt);
+		break;
+	}
+}
+
+void CBrick::Reaction(CGameObject* by_another, int action)
+{
+	if (state == STATE_DIE)
+		return;
+
+	switch (action)
+	{
+	case ACTION_DESTROY:
+		this->Delete();
+		break;
+	case ACTION_TOUCH:
+		SetState(BRICK_STATE_TOGGLE);
+		break;
+	}
+
+	TriggerItem();
+}
+
+void CBrick::SetState(int state)
+{
+	if (this->state == state)
+		return;
+
+	this->state = state;
+
+	switch (state)
+	{
+	case STATE_LIVE:
+		break;
+	case BRICK_STATE_TOGGLE:
+		break;
+	case STATE_DIE:
+		vy = 0.0f;
+		break;
+	}
+}
+
+void CBrick::TriggerItem()
+{
+	switch (itemID)
+	{
+	case OBJECT_TYPE_PBUTTON:
+	{
+		item = new CPButton(x, y - BRICK_WIDTH);
+		break;
+	}
+	default: return;
+	}
+
+	item->Reaction(this, ACTION_ATTACK);
+	LPPLAYSCENE ps = (LPPLAYSCENE)CGame::GetInstance()->GetCurrentScene();
+	ps->Add(item);
+}
+
+void CBrick::Shaking(DWORD dt)
+{
+	//Accelerate(0.0f, GAME_GRAVITY, dt);
+	if (!opposite) y += BRICK_SHAKE_VY * dt;
+	else y -= BRICK_SHAKE_VY * dt;
+
+	if (y <= origin_y - BRICK_WIDTH / 2)
+	{
+		opposite = true;
+	}
+	if (y >= origin_y)
+	{
+		y = origin_y;
+		opposite = false;
+		if (itemID != 0) SetState(STATE_DIE);
+		else SetState(STATE_LIVE);
+	}
 }
--- NES-Super-Mario-Bros-3/Brick.h
+++ NES-Super-Mario-Bros-3/Brick.h
@@ -1,18 +1,43 @@
 #pragma once
 
-#include "QuestionBlock.h"
+#include "Block.h"
 #include "Animation.h"
 #include "Animations.h"
 
 #define ID_ANI_BRICK 10000
+#define ID_ANI_BRICK_DIE 12001
 
 #define BRICK_WIDTH 16.0f
 #define BRICK_BBOX_WIDTH 16.0f
 #define BRICK_BBOX_HEIGHT 16.0f
 
-class CBrick : public CQuestionBlock {
+#define BRICK_SHAKE_VY -0.16f
+
+#define BRICK_STATE_TOGGLE 10
+
+#define ITEM_TYPE_NOTHING 0
+
+class CBrick : public CBlock {
+	//Containing
+	int itemID;
+	CGameObject* item;
+
+	//Shaking
+	float origin_y;
+	bool opposite = false;
 public:
-	CBrick(float x, float y);
+	CBrick(float x, float y, int itemID = ITEM_TYPE_NOTHING);
 	//GameObject co ham update roi! "virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects = NULL) {};"
 	//GameObject co ham render roi! "virtual void Render();"
+	void Render();
+	void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
+	void InPhase(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
+
+	void Reaction(CGameObject* by_another, int action);
+	void SetState(int state);
+	int GetItemID() { return itemID; }
+
+	void TriggerItem();
+
+	void Shaking(DWORD dt);
 };
